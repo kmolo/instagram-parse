20cb0db0f33b9f5c08ffb4bf4a36da6eb2003eca
Katherine Moloney <kmoloney@mail.ccsf.edu>
2016-02-22 22:56:59 -0800

    create IBOutlets for TextFields + IBActions for Buttons
    IBOutlets: usernameField + passwordField
    IBActions: onSignIn + onSignUp

--- instagram-parse/LoginViewController.swift
+++ instagram-parse/LoginViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +21,13 @@ class LoginViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func onSignIn(sender: AnyObject) {
+    }
+    
+    @IBAction func onSignUp(sender: AnyObject) {
     }
 
 
