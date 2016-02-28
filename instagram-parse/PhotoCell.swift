31833a3d13584d6d2dd365989e1c97a6e765e0ba
Katherine Moloney <kmoloney@mail.ccsf.edu>
2016-02-27 20:17:17 -0800

    create new PhotoCell subclassing UITableViewCell in group Views

---
+++ instagram-parse/PhotoCell.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoCell.swift
+//  instagram-parse
+//
+//  Created by mny on 2/27/16.
+//  Copyright © 2016 ccsf. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
